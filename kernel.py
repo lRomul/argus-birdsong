58e88a4e4b1d550885fff490115101277ad11bcc
ruslan <ruslan1123@gmail.com>
2020-08-04 14:32:59 +0300

    feat: Add kernel script

---
+++ kernel.py
@@ -0,0 +1,18 @@
+import os
+
+
+EXPERIMENT = "stride_002"
+
+
+def run(command):
+    os.system("export PYTHONPATH=${PYTHONPATH}:/kaggle/working && "
+              f"export KERNEL_MODE=predict && " + command)
+
+
+run("pip install /kaggle/input/argus-birdsong-dataset/pydantic-1.6.1-py36.py37.py38-none-any.whl")
+run("pip install /kaggle/input/argus-birdsong-dataset/pytorch_argus-0.1.1-py3-none-any.whl")
+run("pip install /kaggle/input/argus-birdsong-dataset/timm-0.1.30-py3-none-any.whl")
+run("cp -r /kaggle/input/argus-birdsong-dataset/argus-birdsong/* .")
+run("cp -r /kaggle/input/birdcall-check/* ./data/")
+
+run(f"python predict_kernel.py --experiment {EXPERIMENT}")
