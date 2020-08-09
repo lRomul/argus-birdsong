c53c5932abdea28de866e4dfc27530c748f559a9
ruslan <ruslan1123@gmail.com>
2020-08-09 17:11:46 +0300

    feat: Install resnest in kernel

--- data/birdsong-deps/download.sh
+++ data/birdsong-deps/download.sh
@@ -4,7 +4,8 @@ rm -rf ./*.whl
 pip download --no-deps \
     pydantic==1.6.1 \
     timm==0.1.30 \
-    pytorch-argus==0.1.1
+    pytorch-argus==0.1.1 \
+    resnest==0.0.5
 
 rm -rf ./argus-birdsong
 git clone https://github.com/lRomul/argus-birdsong && cd argus-birdsong && git checkout "$2" && cd ..
--- kernel.py
+++ kernel.py
@@ -9,6 +9,7 @@ def run(command):
 run("pip install /kaggle/input/argus-birdsong-dataset/pydantic-1.6.1-py36.py37.py38-none-any.whl")
 run("pip install /kaggle/input/argus-birdsong-dataset/pytorch_argus-0.1.1-py3-none-any.whl")
 run("pip install /kaggle/input/argus-birdsong-dataset/timm-0.1.30-py3-none-any.whl")
+run("pip install /kaggle/input/argus-birdsong-dataset/resnest-0.0.5-py3-none-any.whl")
 run("cp -r /kaggle/input/argus-birdsong-dataset/argus-birdsong/* .")
 run("cp -r /kaggle/input/birdcall-check/* ./data/")
 
